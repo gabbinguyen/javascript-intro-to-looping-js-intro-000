0d9cc7538d3e623450c44011ca62c578a6a3927c
magical-builder-5600 <gabbinguyen@gmail.com>
2020-09-17 21:31:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,12 @@ function whileLoop(n) {
   }
   return 'done';
 }
+
+function doWhileLoop(num) {
+  var i = 0;
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  do {console.log("I run once regardless.")}
+} while (incrementalVariable() < 11);
